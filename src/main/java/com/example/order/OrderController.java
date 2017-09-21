e68ef4beb5d3f804f1e60e6cd46914a2dab83f6a
svarpe <sagar131@gmail.com>
2017-09-21 17:56:51 +0200

    basic setup with Redis and enabled security

--- src/main/java/com/example/order/OrderController.java
+++ src/main/java/com/example/order/OrderController.java
@@ -1,18 +1,21 @@
-package com.example.user;
+package com.example.order;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 /**
  * Created by svarpe on 21.09.17.
  */
-@RestController("user")
-public class UserController {
+@RestController
+@RequestMapping("order")
+public class OrderController {
 
 
     @RequestMapping(method = RequestMethod.GET)
-    public String index(){
-        return "Hallo";
+    public String index(Principal principal){
+        return "Hallo from Order Service to  " +principal.getName();
     }
 }
